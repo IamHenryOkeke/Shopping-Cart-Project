e688df79b75721f7ed8851c95c9fb1da0c542219
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-12 17:43:46 +0100

    implemented shopping page for project

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import useFetch from '../hooks/useFetch'
+import Item from '../components/Item'
+
+
 
 const Shop = () => {
+  const { data, error, loading } = useFetch();
+
+  const list = ["All", "jewelery", "electronics", "women's clothing", "men's clothing"];
+
   return (
-    <div>
-      Hello from shop
-    </div>
+    <main className="mx-6 md:mx-14 lg:mx-28">
+      {
+        loading && <p className='text-center'>Loading....</p>
+      }
+
+      {
+        error && <p>{error}</p>
+      }
+
+      {
+        data && (
+          <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-6 md:gap-10'>
+            {data.map(item => (
+              <Item key={item.id} {...item}/>
+            ))}
+          </div>
+        ) 
+      }
+      
+    </main>
   )
 }
 
