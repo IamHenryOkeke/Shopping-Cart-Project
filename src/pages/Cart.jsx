eb669f1931e61aeae0eac64aae99277df1391ba6
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-13 15:39:54 +0100

    implemented cart page and refactored shop page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
+import CartItems from "../components/CartItems";
+import { useEffect } from "react";
+import { calculateTotal } from "../features/shopSlice";
 
-const Checkout = () => {
+
+const Cart = () => {
+
+  const { numberOfItemsInCart, cartItems, total } = useSelector((state) => state.shop)
+
+  const dispatch = useDispatch(); 
+
+  useEffect(() => {
+     dispatch(calculateTotal(cartItems))
+  }, [])
+
+  if(numberOfItemsInCart < 1) {
+    return (
+      <div className="mx-6 md:mx-14 lg:mx-28">
+        <h1 className='text-center font-bold text-2xl'>Your cart is empty</h1>
+      </div>
+    )
+  }
   return (
-    <div>
-      Hello from cart
+    <div className="mx-6 lg:mx-28">
+      <h1 className='text-center font-bold text-2xl mb-5'>Your Cart</h1>
+      <div className='grid gap-4 lg:px-48'>
+            {cartItems.map(item => (
+              <CartItems key={item.id} {...item}/>
+            ))}
+      </div>
+
+      <div className="flex justify-center gap-8 mt-5 lg:text-2xl">
+        <p>
+          Total:
+        </p>
+        <p>
+          $ {total.toFixed(2)}
+        </p>
+      </div>
     </div>
   )
 }
 
-export default Checkout
+export default Cart
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,4 @@
-import Item from '../components/Item'
+import Item from '../components/ShopItem'
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getData } from '../features/shopSlice';
@@ -14,22 +14,26 @@ const Shop = () => {
       dispatch(getData());
   }, [])
 
+  if(isLoading) {
+    return (
+      <p className='text-center'>Loading....</p>
+    )
+  }
+
+  if(error) {
+    return (
+      <div className='flex flex-col gap-5'>
+          <p className='text-center'>{error}</p>
+          <button onClick={() => {
+            dispatch(getData());
+          }} className='transition-all self-center ease-in-out delay-150 duration-300 bg-[#546b56] hover:bg-black px-8 rounded-md py-2 text-white font-bold'>Add to cart</button>
+      </div> 
+    )
+  }
+
   return (
     <main className="mx-6 md:mx-14 lg:mx-28">
       { shopItems && <Filter/> }
-
-      {
-        isLoading && <p className='text-center'>Loading....</p>
-      }
-
-      {
-        error && <div className='flex flex-col gap-5'>
-                    <p className='text-center'>{error}</p>
-                    <button onClick={() => {
-                      dispatch(getData())
-                    }} className='transition-all self-center ease-in-out delay-150 duration-300 bg-[#546b56] hover:bg-black px-8 rounded-md py-2 text-white font-bold'>Add to cart</button>
-                  </div> 
-      }
 
       {
         shopItems && (
