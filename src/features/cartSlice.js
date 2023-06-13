c74fc009a415c08a8cb6482d96e22eef902f4a6f
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-13 15:42:33 +0100

    finished basic functionality for redux slice

--- src/features/cartSlice.js
+++ src/features/cartSlice.js
@@ -1,14 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
 const initialState = {
+  cartData : [],
   cartItems: [],
+  numberOfItemsInCart : 0
 }
+
+export const setCartDataState = createAsyncThunk(
+  "names/addWithThunk",
+  async (_, thunkAPI) => {
+    return thunkAPI.getState().shop.data;
+  }
+);
 
 export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-
+    
+  },
+  extraReducers : {
+    [setCartDataState.pending]:(state) => {
+      state.shopItems = [];
+    },
+    [setCartDataState.fulfilled]: (state, action) => {
+      console.log(action.payload);
+     
+      state.cartData = action.payload;
+    },
+    [setCartDataState.rejected]: (state,action) => {
+      // console.log(action)
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 })
 
--- src/features/shopSlice.js
+++ src/features/shopSlice.js
@@ -1,20 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit'
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const initialState = {
   data: [],
   isLoading: true,
   error : null,
-  shopItems : []
+  shopItems : [],
+  cartItems : [],
+  numberOfItemsInCart : 0,
+  total : 0
 }
 
 const url = "https://fakestoreapi.com/products";
 
+const checkCart = (item, array) =>{
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].id === item.id) {
+            return false;
+        }
+    }
+    return true;
+}
+
 export const getData = createAsyncThunk('cart/getItems', async (_, thunkAPI) => {
     try {
       const response = await axios(url);
-      console.log(response.data)
       return response.data
     } catch (error) {
       console.log(error)
@@ -26,6 +36,24 @@ export const shopSlice = createSlice({
   name: 'shop',
   initialState,
   reducers: {
+    increaseAmount : (state, action) => {
+      const id = action.payload;
+      const result = state.cartItems.find((item)=> {
+        return item.id === id
+      });
+      result.amount += 1;
+    },
+
+    decreaseAmount : (state, action) => {
+      const id = action.payload;
+      const result = state.cartItems.find((item)=> {
+        return item.id === id
+      });
+      if(result.amount !== 0){
+        result.amount -= 1;
+      }
+    },
+
     filterItems : (state, action) => {
       state.shopItems = state.data
       if(action.payload !== "All"){
@@ -34,6 +62,25 @@ export const shopSlice = createSlice({
         })
       }
     },
+    addItemToCart : (state, action) => {
+      const id = action.payload
+      // console.log(id)
+      const result = state.data.find((item)=> {
+         return item.id === id
+      });
+      if(checkCart(result, state.cartItems)){
+        result.amount = 1;
+        state.cartItems.push(result)
+        state.numberOfItemsInCart = state.cartItems.length
+      }
+    },
+    calculateTotal : (state) => {
+      state.total = 0
+      for (let i = 0; i < state.cartItems.length; i++) {
+        state.total += (state.cartItems[i].price * state.cartItems[i].amount);
+      }
+    }
+    
   },
   extraReducers : {
     [getData.pending]:(state) => {
@@ -57,6 +104,6 @@ export const shopSlice = createSlice({
 })
 
 // Action creators are generated for each case reducer function
-export const { filterItems } = shopSlice.actions
+export const { increaseAmount, decreaseAmount, filterItems, addItemToCart, calculateTotal } = shopSlice.actions
 
 export default shopSlice.reducer
