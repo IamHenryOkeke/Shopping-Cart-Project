e6bd21e5a55f07fa099c7a760c2a9499b0fb0fe4
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-14 12:08:25 +0100

    added checkout button

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,25 +1,43 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { filterItems } from '../features/shopSlice';
+import { Link } from 'react-router-dom';
 
 const Filter = () => {
   const dispatch = useDispatch();
-  // const [value, setValue] = useState("");
+  const { numberOfItemsInCart } = useSelector((state) => state.shop)
+
   const list = ["All", "jewelery", "electronics", "women's clothing", "men's clothing"];
   
   return (
-    <div className="flex gap-5 items-center mb-5">
-        <p className='text-xl'>Category:</p>
-        <select className='bg-[#546b56] text-white font-semibold pl-2 py-1 rounded' onChange={(e) => {
-          dispatch(filterItems(e.target.value))
-        }}>
+    <div className="flex justify-between mb-5">
+        <div className='flex gap-5 items-center'>
+            <p className='text-xl'>Category:</p>
+            <select className='bg-[#546b56] text-white font-semibold pl-2 py-1 rounded' onChange={(e) => {
+              dispatch(filterItems(e.target.value))
+            }}>
+              {
+                list.map((item, index) => {
+                  return (
+                    <option key={index} value={item}>{item}</option>
+                  )
+                })
+              }
+            </select>
+        </div>
+        <div>
           {
-            list.map((item, index) => {
-              return (
-                <option key={index} value={item}>{item}</option>
-              )
-            })
+            numberOfItemsInCart > 0 ? (
+              <Link to="/cart">
+                  <button className='transition-all ease-in-out delay-150 duration-300 bg-[#546b56] hover:bg-black px-8 rounded-md py-2 text-white font-bold'>Proceed to checkout</button>
+              </Link>
+            ) : (
+              <div>
+
+              </div>
+            )
           }
-        </select>
+        </div>
+        
       </div>
   )
 }
