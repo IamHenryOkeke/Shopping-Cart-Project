7a7215b574e403944dcec4f2e888635397f318d7
Henry Okeke <iamhenryokeke@gmail.com>
2023-06-14 12:24:45 +0100

    ui fixes

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -9,10 +9,10 @@ const Filter = () => {
   const list = ["All", "jewelery", "electronics", "women's clothing", "men's clothing"];
   
   return (
-    <div className="flex justify-between mb-5">
-        <div className='flex gap-5 items-center'>
-            <p className='text-xl'>Category:</p>
-            <select className='bg-[#546b56] text-white font-semibold pl-2 py-1 rounded' onChange={(e) => {
+    <div className="flex justify-between items-end mb-5">
+        <div className='flex gap-2 md:gap-5 flex-col md:flex-row md:items-center'>
+            <p className='text-sm md:text-xl'>Category:</p>
+            <select className='bg-[#546b56] text-sm md:text-xl text-white font-semibold pl-2 py-1 rounded' onChange={(e) => {
               dispatch(filterItems(e.target.value))
             }}>
               {
@@ -27,8 +27,8 @@ const Filter = () => {
         <div>
           {
             numberOfItemsInCart > 0 ? (
-              <Link to="/cart">
-                  <button className='transition-all ease-in-out delay-150 duration-300 bg-[#546b56] hover:bg-black px-8 rounded-md py-2 text-white font-bold'>Proceed to checkout</button>
+              <Link to="/cart" className='text-sm md:text-xl'>
+                  <button className='transition-all ease-in-out delay-150 duration-300 bg-[#546b56] hover:bg-black px-2 py-1 md:px-8 rounded-md md:py-2 text-white font-bold'>Proceed to checkout</button>
               </Link>
             ) : (
               <div>
