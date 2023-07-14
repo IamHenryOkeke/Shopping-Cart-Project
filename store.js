e939d232c717b50d89da6989dfc2fb6cc874535f
Henry Okeke <iamhenryokeke@gmail.com>
2023-07-14 21:22:00 +0100

    removed unused slice file

--- store.js
+++ store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import shopReducer from './src/features/shopSlice'
-import cartReducer from './src/features/cartSlice'
 
 
 export const store = configureStore({
   reducer: {
-    shop : shopReducer,
-    cart : cartReducer
+    shop : shopReducer
   },
 })
